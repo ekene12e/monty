8dea03986c75256aca5c3c8c4ddac53e759d8bcb
ekene12e <ekenestanekeson@gmail.com>
2022-06-14 22:43:55 +0100

    add

--- readfile.c
+++ readfile.c
@@ -1,9 +1,4 @@
 #include "monty.h"
-#include <stdio.h>
-#define  _GNU_SOURCE
-#define  _POSIX_C_SOURCE 200809L
-char *array_of_read_lines[1000] = {NULL};
-
 /**
  * read_line - reads a line from a file 
  * @filename: the file to be read
@@ -13,22 +8,26 @@ char **read_line(char *filename)
 {
     FILE *fd;
     int n = 0, i;
-    
+    char *array_of_read_lines[20];
     size_t bufsize = 1024;
 
     fd = fopen(filename, "r");
     if (fd == NULL)
         fprintf(stderr, "Error openening %s\n", filename);
         exit(EXIT_FAILURE);
+    array_of_read_lines[0] = (char *)malloc(bufsize);
 
     while (fgets(array_of_read_lines[n], bufsize, fd) != NULL)
         {
+            if (n > 0)
+                array_of_read_lines[n] = (char *)malloc(bufsize);
+            printf("%s\n", array_of_read_lines[n]);
+
             n++;
         }
-    
     for(i = 0; i  < n; i++)
-        printf("s\n", array_of_read_lines[i]);
-    return (array_of_read_lines);
+        printf("%s\n", array_of_read_lines[i]);
+    /*return (array_of_read_lines);*/
 }
 
 int main (int ac, char **av)
